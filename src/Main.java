8626dc85750db02e16ff490b55586bfb94132a0b
Spiros_Vardis <41401981+spirosvar@users.noreply.github.com>
2022-11-28 23:12:21 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    //this is the entry point of our app -- every time it will be executed this is the starting point
+    public static void main(String[] args) {
+
+        System.out.println("Test that everything's running!");
+    }
+}
